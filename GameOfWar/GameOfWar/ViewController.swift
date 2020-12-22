715ddce7d92a09dfed95c86b3e51cc17b449992a
Matt Dolan <mdolan.code@gmail.com>
2020-12-22 07:58:47 -0800

    Commit 1: created project.

---
+++ GameOfWar/GameOfWar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfWar
+//
+//  Created by Matt Dolan External macOS on 2020-12-22.
+//  Copyright © 2020 Matt Dolan External macOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
